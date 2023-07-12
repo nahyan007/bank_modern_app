30410e4c8378ad12d2930ef18e8ab3817bfa033d
nahyan007 <shorifrex@gmail.com>
2023-07-12 22:39:59 +0600

    responsive stats

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "../style";
 import { discount, robot } from "../assets";
 import GetStarted from "./GetStarted";
@@ -21,7 +20,7 @@ const Hero = () => {
           </p>
         </div>
 
-        <div className=" flex flex-row justify-between items-center w-full border border-blue-600 ">
+        <div className=" flex flex-row justify-between items-center w-full  ">
           <h1 className=" flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px] ">
             The Next <br className="sm:block hidden" />{" "}
             <span className="text-gradient">Generation</span>{" "}
@@ -31,11 +30,11 @@ const Hero = () => {
             <GetStarted />
           </div>
         </div>
-        <h1 className=" font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full border-2 border-green-900">
+        <h1 className=" font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
           Payment Method
         </h1>
         <p
-          className={`${styles.paragraph} max-w-[470px] mt-5 border border-red-900 `}
+          className={`${styles.paragraph} max-w-[470px] mt-5 `}
         >
           Our team of experts uses a methodology to identify the credit cards
           most likely to fit your needs. We examine annual percentage rates,
