45938738767e2676ae77fc2e02ad1ebaa1904144
nahyan007 <shorifrex@gmail.com>
2023-07-16 01:07:52 +0600

    clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { clients } from "../constants";
+import styles from "../style";
 
-function Clients() {
-  return (
-    <div>Clients</div>
-  )
-}
+const Clients = () => (
+  <section className={` ${styles.flexCenter} my-4 `}>
+    <div className={` ${styles.flexCenter} flex-wrap w-full `}>
+      {clients.map((client) => (
+        <div key={client.id} className={` flex-1 ${styles.flexCenter}  sm:min-w-[192px] min-w-[120px]   `}>
+          <img src={client.logo} alt={client.logo} className=" sm:w-[192px] w-[100px] object-contain  feedback-card px-6 py-2 cursor-pointer rounded-full " />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
-export default Clients
+export default Clients;
